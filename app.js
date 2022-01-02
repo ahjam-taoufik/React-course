79894bf6d7fec8e104fb26c7d88774e7d806b4e9
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-01-02 18:45:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
 
-// class Compose extends React.Component{
-//     render(){
-//        // console.log(this.props);
-//         return <h1>Welcome {this.props.name}</h1>
-//     }
-// }
-
-// ReactDOM.render(
-//     <div>
-//        <Compose name='ali'> to your class</Compose>
-//        <Compose name='mostapha'> to your class</Compose>
-//     </div>
-// ,document.querySelector('#app'))
-
-
-
-//==========================================================================
-
-// class Compose extends React.Component{
-//     render(){
-// =====// console.log(this.props);
-//         return <h1>Welcome {this.props.name}</h1>
-//     }
-// }
-// function Main(){
-//     return   <div>
-//        <Compose name='ali'> to your class</Compose>
-//        <Compose name='mostapha'> to your class</Compose>
-//     </div>
-// }
-// ReactDOM.render(
-//       <Main/>
-// ,document.querySelector('#app'))
-
-//===============================================================
-
-
 
 class Compose extends React.Component{
     render(){
