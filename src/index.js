40a82bc415cb7cd8e9ab167438c3d40d285f4834
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-03 12:17:00 +0100

    Revision_useState_useEffect

--- src/index.js
+++ src/index.js
@@ -1,25 +1,53 @@
 
 import {render} from 'react-dom';
-import Compter from './app'
+import React, { useEffect, useState } from 'react'
+
+function useIncrement(init=0,step=1){
+     const [state, setstate] = useState(init)
+       function setIncrement(){
+          setstate(state=>state+step)
+}return [state,setIncrement]
+}
+
+function useVisible(bool=true){
+        const [isVisible, setisVisible] = useState(bool)
+        function toggle(){
+          setisVisible(isVisible=>!isVisible)
+        }
+  return[isVisible, toggle]
+}
+
+function useAutoIcrement(initialState=0, step=1, speed=1000){
+           const [state, setstate] = useIncrement(initialState,step)
+           useEffect(() => {
+            const time=setInterval(() => {
+              setstate()
+              },speed)
+              return () => {
+                 clearInterval(time)
+              }
+        }, [])
+           return state
+}
+
+function Compteur() {
+            const autoIncrement = useAutoIcrement()
+       
+        const [count, setincrement] = useIncrement()
+       return <>
+             <button onClick={setincrement}>increment {count}</button>
+              <h1>{autoIncrement}</h1>
+
+       </>
+}
+
+function App(){
+            const [isVisible, setstate] = useVisible(true)        
+    return <div>
+              <input type="checkbox" onChange={setstate} checked={isVisible} />
+              {isVisible && <Compteur/>}
+           </div>
+}
 
 
-
-setTimeout(()=>{
-  render(   
-    <div> 
-      salam
-    </div>
-   ,
-   document.getElementById('root')
- );
- 
-},5000)
-
-
-render(   
-   <div> 
-     <Compter/>
-   </div>
-  ,
-  document.getElementById('root')
-);
+render(<App/>,document.getElementById('root'));
