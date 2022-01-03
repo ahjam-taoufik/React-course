0c4e6f7c5f957306c31e6251234d7e689b0f4c63
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-03 21:22:24 +0100

    Accessing Context P1

--- src/coponents/Booklist.js
+++ src/coponents/Booklist.js
@@ -1,15 +1,21 @@
 
 import React from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 
 class Booklist extends React.Component {
+    static contextType=ThemeContext;
+  
     render() { 
+          const{dark,islightTheme,light}=this.context;
+          const theme=islightTheme?light:dark;
+          //console.log(theme);
         return (
-            <div className="book-list">
+            <div className="book-list" style={{background:theme.bg, color:theme.syntax}}>
                 <ul>
-                    <li>ths first book</li>
-                    <li>the gist</li>
-                    <li>the best one</li>
+                    <li style={{background:theme.ui}}>ths first book</li>
+                    <li style={{background:theme.ui}}>the gist</li>
+                    <li style={{background:theme.ui}}>the best one</li>
                    
                 </ul>
               </div>
--- src/coponents/Navbar.js
+++ src/coponents/Navbar.js
@@ -1,10 +1,20 @@
-
 import React from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 
 class Navbar extends React.Component {
+    static contextType=ThemeContext;
+  
     render() { 
+          //console.log(this.context);
+         const{dark,islightTheme,light}=this.context
+        //  console.log(dark);
+        //  console.log(islightTheme);
+        //  console.log(light);
+        const theme=islightTheme?light:dark;
+        //console.log(theme);
         return(
-              <nav>
+              <nav style={{background:theme.ui, color:theme.syntax}}>
                 <h1>Context App</h1>
                 <ul>
                     <li>Home</li>
