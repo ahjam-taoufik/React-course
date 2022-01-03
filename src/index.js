5ca55462bd36265273c31d11e787b7bdd8abb63a
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-03 12:52:48 +0100

    Load data from database

--- src/index.js
+++ src/index.js
@@ -2,52 +2,32 @@
 import {render} from 'react-dom';
 import React, { useEffect, useState } from 'react'
 
-function useIncrement(init=0,step=1){
-     const [state, setstate] = useState(init)
-       function setIncrement(){
-          setstate(state=>state+step)
-}return [state,setIncrement]
-}
-
-function useVisible(bool=true){
-        const [isVisible, setisVisible] = useState(bool)
-        function toggle(){
-          setisVisible(isVisible=>!isVisible)
-        }
-  return[isVisible, toggle]
-}
-
-function useAutoIcrement(initialState=0, step=1, speed=1000){
-           const [state, setstate] = useIncrement(initialState,step)
-           useEffect(() => {
-            const time=setInterval(() => {
-              setstate()
-              },speed)
-              return () => {
-                 clearInterval(time)
-              }
-        }, [])
-           return state
-}
-
-function Compteur() {
-            const autoIncrement = useAutoIcrement()
-       
-        const [count, setincrement] = useIncrement()
-       return <>
-             <button onClick={setincrement}>increment {count}</button>
-              <h1>{autoIncrement}</h1>
-
-       </>
-}
 
 function App(){
-            const [isVisible, setstate] = useVisible(true)        
+         const [todos, setTodos] = useState([])
+         const [isLoading, setisLoading] = useState(true)
+          useEffect(() => {
+               (async function () {
+                    const res=await fetch('https://jsonplaceholder.typicode.com/todos?_limit=200')
+                    const data=await res.json()
+                    if (data) {  
+                      setTodos(data)
+                    }
+                    setisLoading(false)
+               })()        
+          }, [])
+
+    if (isLoading.ok) {
+      return <h3>Loading .........</h3>
+    }     
     return <div>
-              <input type="checkbox" onChange={setstate} checked={isVisible} />
-              {isVisible && <Compteur/>}
+                <h2>List Todos</h2>
+              <ul>
+                {todos.map(t=><li key={t.id}>{t.title}</li>)} 
+              </ul>
            </div>
 }
+
 
 
 render(<App/>,document.getElementById('root'));
