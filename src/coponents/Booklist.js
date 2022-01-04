bab849681b3a107b4240954984269a287dc7fc7d
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-04 12:16:08 +0100

    useContext in Booklist

--- src/coponents/Booklist.js
+++ src/coponents/Booklist.js
@@ -1,26 +1,23 @@
 
-import React from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 
-class Booklist extends React.Component {
-    static contextType=ThemeContext;
-  
-    render() { 
-          const{dark,islightTheme,light}=this.context;
-          const theme=islightTheme?light:dark;
-          //console.log(theme);
-        return (
-            <div className="book-list" style={{background:theme.bg, color:theme.syntax}}>
-                <ul>
-                    <li style={{background:theme.ui}}>ths first book</li>
-                    <li style={{background:theme.ui}}>the gist</li>
-                    <li style={{background:theme.ui}}>the best one</li>
-                   
-                </ul>
-              </div>
-        ) 
-    }
+const Booklist = () => {
+    const{dark,islightTheme,light}=useContext(ThemeContext)
+    const theme=islightTheme?light:dark;
+    //console.log(theme);
+  return (
+      <div className="book-list" style={{background:theme.bg, color:theme.syntax}}>
+          <ul>
+              <li style={{background:theme.ui}}>ths first book</li>
+              <li style={{background:theme.ui}}>the gist</li>
+              <li style={{background:theme.ui}}>the best one</li>
+             
+          </ul>
+        </div>
+  ) 
 }
  
 export default Booklist;
+
